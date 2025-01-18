7403aa16443ecf4a87e1760845a8756e76192b85
Evelin Maripuu <evelin.marit@gmail.com>
2025-01-18 18:04:41 +0200

    HomeControllerTests added

--- KooliProjekt.UnitTests/ControllerTests/HomeControllerTests.cs
+++ KooliProjekt.UnitTests/ControllerTests/HomeControllerTests.cs
@@ -1,5 +1,8 @@
 ﻿using KooliProjekt.Controllers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlTypes;
+using System.Diagnostics;
 using Xunit;
 
 namespace KooliProjekt.UnitTests.ControllerTests
@@ -18,6 +21,49 @@ namespace KooliProjekt.UnitTests.ControllerTests
             // Assert
             Assert.NotNull(result);
             Assert.True(result.ViewName == "Index" ||
+                        string.IsNullOrEmpty(result.ViewName));
+        }
+        [Fact]
+        public void Privacy_should_return_index_view()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Privacy() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.ViewName == "Privacy" ||
+                        string.IsNullOrEmpty(result.ViewName));
+        }
+        [Fact]
+        public void Error_should_return_index_view_with_httpcontext()
+        {
+            // Arrange
+            var controller = new HomeController();
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            // Act
+            var result = controller.Error() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.ViewName == "Error" ||
+                        string.IsNullOrEmpty(result.ViewName));
+        }
+        [Fact]
+        public void Error_should_return_index_view_with_current_activity()
+        {
+            // Arrange
+            var controller = new HomeController();
+            Activity.Current = new Activity("activity").Start();
+            // Act
+            var result = controller.Error() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.ViewName == "Error" ||
                         string.IsNullOrEmpty(result.ViewName));
         }
     }
--- KooliProjekt/Controllers/HomeController.cs
+++ KooliProjekt/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace KooliProjekt.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -26,7 +19,18 @@ namespace KooliProjekt.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            string requestId;
+            if(Activity.Current != null)
+            {
+                requestId = Activity.Current.Id;
+            }
+            else
+            {
+                requestId = HttpContext.TraceIdentifier;
+            }
+            return View(new ErrorViewModel { RequestId = requestId });
         }
     }
 }
